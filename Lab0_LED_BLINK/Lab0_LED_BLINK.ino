91d77bbfc9754929aa2522ebf0b99bbecc63e3b7
nurthin aziz <nurthinaziz@csus.edu>
2016-09-26 19:44:32 -0700

    Finished part 2

--- Lab0_LED_BLINK/Lab0_LED_BLINK.ino
+++ Lab0_LED_BLINK/Lab0_LED_BLINK.ino
@@ -1,6 +1,8 @@
 void setup()      //Setup to configure pins
 {
    const int ledPin = 13;
+   int pushPin = 12;
+   pinMode(pushPin, INPUT);
    pinMode(ledPin, OUTPUT);   
    Serial.begin(9600);   //Initializes 
 }
@@ -11,4 +13,8 @@ void loop()      //Equivalent of a infinite loop
    delay(1000);
    digitalWrite(ledPin, LOW);
    delay(1000);
+   if(ledPin)
+   {
+      Serial.println("It's on!");
+   }
 }
